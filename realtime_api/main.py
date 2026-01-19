95dbbd5e4f04b433ee90d1ae77831c0868c4ecd7
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2026-01-20 00:00:10 +0100

    feat: Added timezone

--- realtime_api/main.py
+++ realtime_api/main.py
@@ -1,12 +1,29 @@
 from fastapi import FastAPI
 from datetime import datetime
-
+import zoneinfo
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/time")
-async def time():
-    return {"time": datetime.now()}
+timezone_countries = {
+    "US": "America/New_York",
+    "CA": "America/Toronto",
+    "MX": "America/Mexico_City",
+    "GB": "Europe/London",
+    "FR": "Europe/Paris",
+    "DE": "Europe/Berlin",
+    "IT": "Europe/Rome",
+    "ES": "Europe/Madrid",
+    "NL": "Europe/Amsterdam",
+}
+
+@app.get("/time/{iso_code}")
+async def time(iso_code: str):
+    iso = iso_code.upper()
+    timezone_string = timezone_countries.get(iso)
+    if not timezone_string:
+        return {"error": "Invalid ISO code"}
+    tz = zoneinfo.ZoneInfo(timezone_string)
+    return {"time": datetime.now(tz)}
